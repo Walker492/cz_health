f86e39db648bad1af3b066897016fd713a6b14ff
wenjr <238239152@qq.com>
2020-11-07 16:34:24 +0800

    移动端套餐缓存修改

--- health_mobile/src/main/java/com/itheima/health/controller/SetmealMobileController.java
+++ health_mobile/src/main/java/com/itheima/health/controller/SetmealMobileController.java
@@ -1,14 +1,18 @@
 package com.itheima.health.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
 import com.itheima.health.Utils.QiNiuUtils;
 import com.itheima.health.constant.MessageConstant;
 import com.itheima.health.entity.Result;
 import com.itheima.health.pojo.Setmeal;
 import com.itheima.health.service.SetmealService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import java.util.List;
 
@@ -23,7 +27,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/setmeal")
 public class SetmealMobileController {
-
+    @Autowired
+    private JedisPool jedisPool;
     @Reference
     private SetmealService setmealService;
 
@@ -33,14 +38,21 @@ public class SetmealMobileController {
      */
     @GetMapping("/getSetmeal")
     public Result getSetmeal(){
-        // 套餐列表
-        List<Setmeal> setmealList = setmealService.findAll();
-        // 前端需要显示图片，要拼接图片的完整路径 java8 stream流操作
-        setmealList.forEach(s -> s.setImg(QiNiuUtils.DOMAIN+s.getImg()));
-        //for (Setmeal s : setmealList) {
-        //    s.setImg(QiNiuUtils.DOMAIN+ s.getImg());
-        //}
-        return new Result(true, MessageConstant.QUERY_SETMEAL_SUCCESS,setmealList);
+        String key = "findAll";
+//        先判断redis中有没有数据
+        Jedis jedis = jedisPool.getResource();
+        String setmeal = jedis.get(key);
+        if (setmeal != null) {
+            List list = JSON.parseObject(setmeal, List.class);
+            return new Result(true, MessageConstant.QUERY_SETMEAL_SUCCESS,list);
+        }else {
+//            不存在话 调用dao层，存到redis，再返回
+            List<Setmeal> setmealList = setmealService.findAll();
+            setmealList.forEach(s -> s.setImg(QiNiuUtils.DOMAIN+s.getImg()));
+//            存入到redis
+            jedis.set(key, JSON.toJSONString(setmealList));
+            return new Result(true, MessageConstant.QUERY_SETMEAL_SUCCESS,setmealList);
+        }
     }
 
     /**
@@ -48,11 +60,24 @@ public class SetmealMobileController {
      */
     @GetMapping("/findDetailById")
     public Result findDetailById(int id){
-        // 调用服务查询
-        Setmeal s = setmealService.findDetailById(id);
-        // 图片的完整路径
-        s.setImg(QiNiuUtils.DOMAIN+s.getImg());
-        return new Result(true, MessageConstant.QUERY_SETMEAL_SUCCESS,s);
+        //判断redis中有没有数据
+        String key = "findDetail"+id;
+        Jedis jedis = jedisPool.getResource();
+//        获取redis中的数据
+        String detail = jedis.get(key);
+        if (detail != null) {
+//            如果存在的话则直接返回
+            Setmeal setmeal = JSON.parseObject(detail, Setmeal.class);
+            return new Result(true, MessageConstant.QUERY_SETMEAL_SUCCESS,setmeal);
+        }else{
+//            不存在的话则调用业务层查询后储存到redis中再返回
+            // 调用服务查询
+            Setmeal s = setmealService.findDetailById(id);
+            // 图片的完整路径
+            s.setImg(QiNiuUtils.DOMAIN+s.getImg());
+            jedis.set(key,JSON.toJSONString(s));
+            return new Result(true, MessageConstant.QUERY_SETMEAL_SUCCESS,s);
+        }
     }
 
     /**
